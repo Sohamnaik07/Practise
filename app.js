293813fda8b83efc489e8d6e41b3871f55d970fc
Sohamnaik07 <146619256+Sohamnaik07@users.noreply.github.com>
2026-08-25 12:02:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('<h1>Deployed via Docker + GitHub Actions + EC2  + Soham</h1>');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 ///////
 app.listen(8080, () => console.log('App running on port 8080'));
